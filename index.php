8496af440cd245d849936b732dccc700e0829b7e
@MikeRzhevsky <39048059+MikeRzhevsky@users.noreply.github.com>
2018-07-19 18:48:15 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
-<?php
-/*
-@MikeRzhevsky miker.ru@gmail.com
- */
-require dirname(__FILE__) . '/autoload.php';
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Список файлов в корневой директории  @MikeRzhevsky miker.ru@gmail.com </title>
+</head>
+<body>
+<form action="/FileList/App/fileListFormPost.php" method="post">
+<table border="2px" cellspacing="0px";>
+    <tr>
+        <th>fieldname</th>
+        <th>filesize</th>
+        <th>filetype</th>
+        <th>moddate</th>
+    </tr>
+    <?php foreach ($files as $file) {?>
+        <tr>
+        <?php  $result = array_unique($file); foreach($result as $fldname => $fldvalues)
+        {
+            if($fldname!='id')
+                echo "<td>".$fldvalues."</td>";
+        }?>
+        </tr>
+    <?php }?>
+</table>
+    <input  type="submit" value="Refresh"/>
+</form>
 
-\App\Models\File::prepareData();
-$files = \App\Models\File::findAll();
-require dirname(__FILE__) . '/Templates/index.php';
-?>
+</body>
+</html>
